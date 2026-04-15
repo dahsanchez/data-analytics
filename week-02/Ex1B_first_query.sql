457a8a9af56f7e0c945b167cfce8577d7ecc4434
dahlia sanchez <dsanchez1@my.yearupunited.org>
2026-04-15 10:47:13 -0700

    Added qeries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,9 @@
 /*
 Sakila is a database used to collect information about actors and flims for use by people
 */
+
+USE sakila;
+SELECT * FROM actor;
+SELECT * FROM actor; -- Retrieved [number] records
+select * from film;
+SELECT * FROM film; -- Retrieved [number] records
