e3f6559889e085fbc411ccc08e4f7fc5d2424743
dahlia sanchez <dsanchez1@my.yearupunited.org>
2026-04-19 10:50:24 -0700

    added Wk2 Ex4C

---
+++ week-02/Ex4C_northwind_extract.sql
@@ -0,0 +1,17 @@
+/* 
+a) What is the name of the table that holds the items Northwind sells?
+The name of the table is products.
+b) What is the name of the table that holds the types/categories of the items 
+Northwind sells?
+The name of the table is categories. */
+
+select * from employees;
+# The employee that makes it look like shes a bird is Margret Peacock.
+
+select * from products;
+# To limit to 10 rows, there isa drop down menu to the left of the "save current statement" button that can limit that amount of rows.
+
+select * from categories;
+# the category id for seafood is 8.
+
+select OrderID, OrderDate,ShipName,ShipCountry from orders;
