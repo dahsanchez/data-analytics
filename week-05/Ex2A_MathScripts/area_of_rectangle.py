f1413ddd1c2dda2bc92eff204ab8956634bac279
Dahlia Sanchez <natisanchez08@yahoo.com>
2026-05-07 12:58:30 -0700

    updated area_of_rectangle

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,16 @@
+# find the area of a rectangle
+# area expresstion is b*h  
+
+# number for base
+base = 8
+
+# number for height
+height = 4
+
+# calculate area with given variables
+area = base * height
+
+#printing side A, side B and total area
+print('Side A is',base)
+print('Side B is',height)
+print('The area of the rectangle is',area)
