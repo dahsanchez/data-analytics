bcfc49cab16e6763d0658ec0f8ae414e00512191
Dahlia Sanchez <natisanchez08@yahoo.com>
2026-05-08 09:01:46 -0700

    added and completed c_to_f

---
+++ week-05/Ex2B_MoreMathScripts/c_to_f.py
@@ -0,0 +1,7 @@
+# Celsius °C to Fahrenheit °F Formula
+
+celsius = int(input('Enter tempature in celsius: '))
+
+fahrenheit = celsius * 1.8 + 32
+
+print(celsius,'°C to fahrenheit is',fahrenheit,'°F')
