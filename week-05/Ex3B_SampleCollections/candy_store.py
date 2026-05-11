e018e9f410ac3427bc38941c7c1d0ceb80866a76
Dahlia Sanchez <natisanchez08@yahoo.com>
2026-05-10 18:09:12 -0700

    added and complted candy_store

---
+++ week-05/Ex3B_SampleCollections/candy_store.py
@@ -0,0 +1,19 @@
+candy = ('lolipops',
+         'sour gummy ropes',
+         'gummy bears'
+        )
+
+flavors = ('Grape',
+           'Strawberyy',
+           'Cherry'
+           )
+
+candy_store = {
+    candy[0] + ' ' + flavors[0],
+    candy[1] +  ' ' + flavors[1],
+    candy[2] + ' ' + flavors[2]
+}
+    
+
+print('Today’s candy options include: ')
+print(candy_store)
