21a79c2e7d46d17d15f97bbb7fdf6cda2ec11d45
Dahlia Sanchez <natisanchez08@yahoo.com>
2026-05-13 18:39:32 -0700

    update

---
+++ week-06/Ex2B_Functions/math_and_stats.py
@@ -0,0 +1,11 @@
+import random 
+import math 
+import statistics
+
+vals_1_100 = range(1,100) 
+vals_sample = random.sample(vals_1_100, 75) 
+vals_choices = random.choices(vals_1_100, k = 200)  
+radius = random.randint(3,10)  
+pi = math.pi 
+
+print(f"Sample of 75 values from 1 to 100: {vals_sample}")
