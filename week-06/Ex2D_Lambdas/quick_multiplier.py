5380433b6c3b7261573ee17f5cdaab46b6d864ce
Dahlia Sanchez <natisanchez08@yahoo.com>
2026-05-14 12:36:45 -0700

    completed quick_multiplier

---
+++ week-06/Ex2D_Lambdas/quick_multiplier.py
@@ -0,0 +1,22 @@
+doubler = lambda n: n * 2
+print(doubler(8))
+print(doubler(-4))
+print(doubler('banana'))
+
+tripler = lambda n: n * 3
+quadrupler = lambda n: n * 4
+quintupler = lambda n: n * 5
+sextupler = lambda n: n * 6
+septupler = lambda n: n * 7
+octupler = lambda n: n * 8
+nonupler = lambda n: n * 9
+decupler = lambda n: n * 10
+
+print(tripler(3))
+print(quadrupler(4))
+print(quintupler(5))
+print(sextupler(6))
+print(septupler(7))
+print(octupler(8))
+print(nonupler(9))
+print(decupler(10))
