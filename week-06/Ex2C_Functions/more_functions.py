d9889ac3dc3240f0ae3868b7f074bdca3caef2c0
Dahlia Sanchez <natisanchez08@yahoo.com>
2026-05-13 21:01:50 -0700

    updated

--- week-06/Ex2C_Functions/more_functions.py
+++ week-06/Ex2C_Functions/more_functions.py
@@ -2,7 +2,7 @@ def display_mailing_label(name,address,city,state,zip):
     return(f'''{name}
 {address}
 {city}{state}{zip} ''')
-print(display_mailing_label('Dahlia Sanchez','123 Main St','Cityville ', 'StateState ', '123456'))
+
 
 def add_numbers(*result):
     total = sum(result)
